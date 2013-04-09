615ba80f90f0e845b2be25c448cd9afd255b1682
Conor Lawless <cnr.lwlss@gmail.com>
2013-04-09 18:08:08 +0100

    Rearranging and renaming files for pypi packaging

--- pypi/setup.py
+++ pypi/setup.py
@@ -1,15 +1,16 @@
 from distutils.core import setup
 setup(name='Colonyzer2',
-      version='1.0.2',
+      version='1.0.43',
       description='Image analysis for microbial cultures growing on solid agar surfaces',
-      long_description='A set of functions for spot location, image segmentation and spot size estimation, suitable for quantifying culture size/cell density for a library of microbial cultures growing on a solid agar surface.',
+      long_description=open('README.txt').read(),
       author='Conor Lawless',
       author_email='conor.lawless@ncl.ac.uk',
       url='http://research.ncl.ac.uk/colonyzer/',
-      py_modules=['Colonyzer2'],
+      py_modules=['colonyzer2'],
+      scripts=['bin/AnalyzeIndependent.py','bin/AnalyzeTimecourse.py'],
       classifiers=[
         'Development Status :: 3 - Alpha',
         'Topic :: Scientific/Engineering :: Image Recognition',
         'Intended Audience :: Science/Research'
-        ]
+        ]      
       )
