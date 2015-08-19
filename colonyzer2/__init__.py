b72d5175a017af45c53994af2fadcb1353f36bcc
Conor Lawless <cnr.lwlss@gmail.com>
2015-08-19 17:50:03 +0100

    Add git commit to version number
    Also make version number appear as colonyzer2.__version__ (standard)

--- colonyzer2/__init__.py
+++ colonyzer2/__init__.py
@@ -1,1 +1,2 @@
+from .version import __version__ as __version__
 from .functions import *
--- setup.py
+++ setup.py
@@ -1,12 +1,43 @@
 from setuptools import setup, find_packages
 from Cython.Build import cythonize
 import numpy
+import os
+import subprocess
+
+def git_version():
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(cmd, stdout = subprocess.PIPE, env=env).communicate()[0]
+        return out
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        GIT_REVISION = out.strip().decode('ascii')
+    except OSError:
+        GIT_REVISION = "Unknown"
+
+    return GIT_REVISION
+
+VERSION='1.0.88'+"."+git_version()[:7]
+f=open('colonyzer2/version.py',"w")
+f.write("__version__='{}'".format(VERSION))
+f.close()
+
 
 # Nice tutorial on packaging python code:
 # http://www.scotttorborg.com/python-packaging/
 
 setup(name='Colonyzer2',
-      version='1.0.88',
+      version=VERSION,
       packages=['colonyzer2','scripts'],
       description='Image analysis for microbial cultures growing on solid agar surfaces',
       long_description=open('README.txt').read(),
