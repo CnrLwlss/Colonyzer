cf3d954fdfe9bc9971e9b72aee63ab4bc3cf7488
Conor Lawless <cnr.lwlss@gmail.com>
2015-12-08 15:31:37 +0000

    Version number

--- colonyzer2/version.py
+++ colonyzer2/version.py
@@ -1,1 +1,1 @@
-__version__='1.1.08.Unknown'
+__version__='1.1.9.Unknown'
--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def git_version():
 
     return GIT_REVISION
 
-version='1.1.08'
+version='1.1.9'
 VERSION=version+"."+git_version()
 f=open('colonyzer2/version.py',"w")
 f.write("__version__='{}'".format(VERSION))
