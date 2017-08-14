c478bbf296ad901b8839157d980d411ec71c5485
CnrLwlss <conor.lawless@ncl.ac.uk>
2017-08-14 17:41:54 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def git_version():
 
     return GIT_REVISION
 
-version='1.1.21'
+version='1.1.22'
 VERSION=version+"."+git_version()
 f=open('colonyzer2/version.py',"w")
 f.write("__version__='{}'".format(VERSION))
